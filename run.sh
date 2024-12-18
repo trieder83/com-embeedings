a7023fd01bf8672b0624d11bf469d92c7df96073
Thomas Rieder <thomas.t.rieder@atos.net>
2024-12-18 11:48:46 +0100

    windows updates

--- run.sh
+++ run.sh
@@ -2,8 +2,14 @@
 
 #docker run -it --rm -p --name embedding embedding
 volume=$PWD/data
-#docker run -p 8888:8888 -it embeddings:latest flask --app createEmbeddings2 run --host 0.0.0.0 --port 8888 -v $volume:/data
-docker run -p 8888:8888 -v $volume:/data -it embeddings:latest flask --app embeddingsolr.py run --host 0.0.0.0 --port 8888 
-#docker run -p 8888:8888 -it embeddings:latest  
+DOCKER=docker
 
-#docker run -p 8888:8888  -it embeddings:latest flask --app embeddingsolr.py run --host 0.0.0.0 --port 8888 
+if [ ! -z $WINDIR ]; then
+  volume=c:\\temp\\data
+  DOCKER="winpty podman"
+fi
+#docker run -p 8888:8888 -it embeddings:latest flask --app createEmbeddings2 run --host 0.0.0.0 --port 8888 -v $volume:/data
+docker run -p 8888:8888  -it embeddings:latest flask --app embeddingsolr.py run --host 0.0.0.0 --port 8888 
+
+#$DOCKER run -p 8888:8888 -v $volume:/data -it embeddings:latest flask --app embeddingsolr.py run --host 0.0.0.0 --port 8888 
+#$DOCKER run -p 8888:8888 -v $volume:/data -it docker.io/trieder83/com-summary:gpu-0.1  flask --app createSummary_mBART.py run --host 0.0.0.0 --port 8888 
---
+++ run_summary.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#docker run -it --rm -p --name embedding embedding
+volume=$PWD/data
+DOCKER=docker
+
+if [ ! -z $WINDIR ]; then
+  volume=c:\\temp\\data_mbart
+  DOCKER="winpty podman"
+fi
+
+$DOCKER run -p 8888:8888 -v $volume:/data -it localhost/trieder83/com-summary:gpu-0.1  flask --app createSummary_mBART.py run --host 0.0.0.0 --port 8888 
